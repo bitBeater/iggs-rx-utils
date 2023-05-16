729a3e5600a3e89917482224c33e354d2d3e0734
alexrr2iggs <alexandru.bunduche@yahoo.com>
2023-05-16 17:04:22 +0200

    add cache operator

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.TakeSubject = exports.beforeComplete = void 0;
+exports.TakeSubject = exports.cache = exports.beforeComplete = void 0;
 var before_complete_1 = require("./operators/before-complete");
 Object.defineProperty(exports, "beforeComplete", { enumerable: true, get: function () { return before_complete_1.beforeComplete; } });
+var cache_1 = require("./operators/cache");
+Object.defineProperty(exports, "cache", { enumerable: true, get: function () { return cache_1.cache; } });
 var TakeSubject_1 = require("./subjects/TakeSubject");
 Object.defineProperty(exports, "TakeSubject", { enumerable: true, get: function () { return TakeSubject_1.TakeSubject; } });
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { beforeComplete } from './operators/before-complete';
+export { cache } from './operators/cache';
 export { TakeSubject } from './subjects/TakeSubject';
